649ed7aa2cdaaf7ba036fcde87fe78ec9891ff19
dmitry <kido.pay@gmail.com>
2019-04-19 10:32:12 +0600

    correct MVC pattern

--- src/Controller.java
+++ src/Controller.java
@@ -1,11 +1,15 @@
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
+import java.awt.print.PrinterException;
+import java.io.*;
 
 
 class Controller implements ActionListener{
     Viewer viewer;
-
+    boolean hasChanges = false;
+    JFileChooser fileChooser = new JFileChooser();;
     Controller(Viewer viewer){
         this.viewer = viewer;
     }
@@ -14,18 +18,133 @@ class Controller implements ActionListener{
         String command = event.getActionCommand();
 
         if(command.equals("Exit")){
-            viewer.exit();
+            exit();
         } else if(command.equals("New")) {
-            viewer.newFile();
+            newFile();
         }else if(command.equals("Open")) {
-            viewer.openFile();
+            openFile();
         }else if(command.equals("Save")) {
-            viewer.saveFile();
+            saveFile();
         }else if(command.equals("SaveAs")) {
-            viewer.saveAsFile();
+            saveAsFile();
         }else if(command.equals("Print")) {
-            viewer.printFile();
+            printFile();
         }
     }
+
+    File file; //File name
+
+    public void newFile(){
+//         проверка на внесение изменений в текущем файле
+//        if (hasChanges){
+//            System.out.println("Файл был изменен!");
+//        }
+//        System.out.println(hasChanges);
+        update("");
+
+    }
+
+    public void openFile(){ //open file
+        // проверка на внесение изменений в текущем файле
+
+        String text = null;
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                "Text files only", "txt");
+        fileChooser.setFileFilter(filter);
+        int ret = fileChooser.showDialog(null, "Открыть файл");
+        if (ret == JFileChooser.APPROVE_OPTION) {
+            file = fileChooser.getSelectedFile();
+            FileInputStream inF;
+            try{
+
+                inF = new FileInputStream(file);
+                BufferedReader in = new BufferedReader(new InputStreamReader(inF, "UTF8"));
+                int words;
+                while ((words = in.read()) != -1){
+                    text = text + (char)words;
+                }
+                update(text);
+
+            }catch(IOException e){
+                System.out.println("Error");
+            }
+        }
+    }
+
+    public void saveFile() { // Save opened file
+
+        try{
+            FileWriter fw = new FileWriter(file);
+            fw.write(sendText().toString());
+            fw.close();
+
+        }catch(IOException e){
+            // Окно с ошибкой доступа к файлу (not work)
+            saveAsFile();
+            System.out.println("Error");
+
+        }catch(NullPointerException e){
+            // Save new document
+            saveAsFile();
+            System.out.println("Error");
+        }
+
+    }
+
+    public void saveAsFile(){ // Save As
+        String text = "test тест";
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                "Text files only", "txt");
+        fileChooser.setFileFilter(filter);
+        int retrival = fileChooser.showSaveDialog(null);
+        if (retrival == JFileChooser.APPROVE_OPTION){
+            try{
+                FileWriter fw = new FileWriter(fileChooser.getSelectedFile());
+                fw.write(sendText().toString());
+                fw.close();
+
+            }catch(IOException e){
+                System.out.println("Error");
+            }
+        }
+
+    }
+
+    public void printFile(){
+        try{
+
+            boolean complite = viewer.textArea.print();
+            if(complite){
+                JOptionPane.showMessageDialog(null, "Done printing", "Information", JOptionPane.INFORMATION_MESSAGE);
+            } else{
+                JOptionPane.showMessageDialog(null, "Printing", "Printer", JOptionPane.ERROR_MESSAGE);
+            }
+
+        }catch(PrinterException e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+
+    public void exit(){
+        System.out.println("GoodBye!");
+        System.exit(1);
+    }
+
+
+    public void update(String text){ //Changes in file
+        hasChanges = true;
+        viewer.textArea.setText(text);
+    }
+
+    public String sendText(){ // Send text to save action
+        String text;
+        text = viewer.textArea.getText();
+        return text;
+    }
+
+
+
+
 
 }
--- src/Viewer.java
+++ src/Viewer.java
@@ -1,17 +1,14 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import javax.swing.ImageIcon;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import java.awt.print.PrinterException;
-import java.io.*;
+
 
 class Viewer {
 
     JTextArea textArea;
-    boolean hasChanges = false;
+
 
     Viewer(){
         Controller controller = new Controller (this);
@@ -19,7 +16,6 @@ class Viewer {
         textArea = new JTextArea();
         JScrollPane scroll = new JScrollPane(textArea);
         JMenuBar jMenuBar = new JMenuBar();
-        JFileChooser fileOpen = new JFileChooser();
         JMenu menuFile = new JMenu("File");
 
 
@@ -68,7 +64,6 @@ class Viewer {
         menuFile.add(saveMenuItem);
         menuFile.add(saveAsMenuItem);
         menuFile.add(new JSeparator());
-//        menuFile.add(pageSetupMenuItem);
         menuFile.add(printMenuItem);
         menuFile.add(new JSeparator());
         menuFile.add(closeMenuItem);
@@ -204,7 +199,6 @@ class Viewer {
 
         frame.setSize(600, 600);
         frame.setLocationRelativeTo(null);
-//        frame.setLocation(100,100);
         frame.add("Center", scroll);
         frame.setJMenuBar(jMenuBar);
         frame.setVisible(true);
@@ -212,118 +206,6 @@ class Viewer {
 
     }
 
-    File file; //File name
-
-    public void newFile(){
-//         проверка на внесение изменений в текущем файле
-//        if (hasChanges){
-//            System.out.println("Файл был изменен!");
-//        }
-//        System.out.println(hasChanges);
-        update("");
-
-    }
-
-    public void openFile(){ //open file
-        // проверка на внесение изменений в текущем файле
-
-        String text = "";
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-        FileNameExtensionFilter filter = new FileNameExtensionFilter(
-                "Text files only", "txt");
-        fileChooser.setFileFilter(filter);
-        int ret = fileChooser.showDialog(null, "Открыть файл");
-        if (ret == JFileChooser.APPROVE_OPTION) {
-            file = fileChooser.getSelectedFile();
-            FileInputStream inF;
-            try{
-
-                inF = new FileInputStream(file);
-                BufferedReader  in = new BufferedReader(new InputStreamReader(inF, "UTF8"));
-                int words;
-                while ((words = in.read()) != -1){
-                    text = text + (char)words;
-                }
-                update(text);
-
-            }catch(IOException e){
-                System.out.println("Error");
-            }
-        }
-    }
-
-    public void saveFile() { // Save opened file
-
-            try{
-                FileWriter fw = new FileWriter(file);
-                fw.write(sendText().toString());
-                fw.close();
-
-            }catch(IOException e){
-                // Окно с ошибкой доступа к файлу (not work)
-                saveAsFile();
-                System.out.println("Error");
-
-            }catch(NullPointerException e){
-                // Save new document
-                saveAsFile();
-                System.out.println("Error");
-            }
-
-    }
-
-    public void saveAsFile(){ // Save As
-        String text = "test тест";
-        JFileChooser fileChooser = new JFileChooser();
-        FileNameExtensionFilter filter = new FileNameExtensionFilter(
-                "Text files only", "txt");
-        fileChooser.setFileFilter(filter);
-        int retrival = fileChooser.showSaveDialog(null);
-        if (retrival == JFileChooser.APPROVE_OPTION){
-            try{
-                FileWriter fw = new FileWriter(fileChooser.getSelectedFile());
-                fw.write(sendText().toString());
-                fw.close();
-
-            }catch(IOException e){
-                System.out.println("Error");
-            }
-        }
-
-    }
-
-    public void printFile(){
-        try{
-
-            boolean complite = textArea.print();
-            if(complite){
-                JOptionPane.showMessageDialog(null, "Done printing", "Information", JOptionPane.INFORMATION_MESSAGE);
-            } else{
-                JOptionPane.showMessageDialog(null, "Printing", "Printer", JOptionPane.ERROR_MESSAGE);
-            }
-
-        }catch(PrinterException e){
-            JOptionPane.showMessageDialog(null, e);
-        }
-    }
-
-    public void exit(){
-        System.out.println("GoodBye!");
-        System.exit(1);
-    }
-
-
-    public void update(String text){ //Changes in file
-        hasChanges = true;
-        textArea.setText(text);
-    }
-
-    public String sendText(){ // Send text to save action
-        String text;
-        text = textArea.getText();
-        return text;
-    }
 
 
 
